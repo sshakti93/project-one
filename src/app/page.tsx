693424a0cb3b734e3cd06c49de08807d7a5cc911
sshakti93 <siddhishakti31@gmail.com>
2024-05-24 15:30:20 +0530

    included a "strong" function to check functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <h1>harini</h1>
+    <h1>
+      <strong>
+        SHAKTI
+      </strong>
+      
+    </h1>
     </main>
   );
 }
